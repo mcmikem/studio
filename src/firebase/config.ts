7c8d75b8bdb8363223f746be7b3c6b72288d0dd8
McMike M <1mark2mike@gmail.com>
2025-10-14 15:59:51 +0000

    firebase storage no default bucket found. set storagebucket. uploads fai

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3977002412-98a01",
   "appId": "1:23348014627:web:05749dbde62724e11dcb96",
+  "storageBucket": "studio-3977002412-98a01.appspot.com",
   "apiKey": "AIzaSyCM_fKRQuoAx2S7C3JPX4SPR5G6vv3uFJA",
   "authDomain": "studio-3977002412-98a01.firebaseapp.com",
   "measurementId": "",
