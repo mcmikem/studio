0fb11ff27e1b065df9d8f58a942aade53aac95e6
McMike M <1mark2mike@gmail.com>
2025-10-22 02:59:20 +0000

    i tried to approve or reject an expense but was told update failed could

--- src/app/(main)/management/expenses/page.tsx
+++ src/app/(main)/management/expenses/page.tsx
@@ -112,7 +112,7 @@ function ExpensesContent() {
     if (!firestore || !currentUser) return;
     const expenseRef = doc(firestore, 'expenses', expense.id);
     try {
-        updateDocumentNonBlocking(expenseRef, { status: status });
+        await updateDocumentNonBlocking(expenseRef, { status: status });
         toast({
           title: `Expense ${status}`,
           description: `The expense report has been marked as ${status.toLowerCase()}.`,
