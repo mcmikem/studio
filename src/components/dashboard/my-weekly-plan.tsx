2e488a2e82140da22799e641f0b8481cdedab060
McMike M <1mark2mike@gmail.com>
2025-10-28 11:16:25 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/dashboard/my-weekly-plan.tsx
+++ src/components/dashboard/my-weekly-plan.tsx
@@ -83,7 +83,9 @@ export function MyWeeklyPlan() {
           ) : (
              <div className="text-center py-4">
                 <p className="text-muted-foreground">You haven't finalized your workplan for this week.</p>
-                <Button asChild variant="link"><Link href="/workplan">Set your plan now!</Link></Button>
+                <Link href="/workplan" passHref>
+                    <Button variant="link">Set your plan now!</Button>
+                </Link>
             </div>
           )}
           {weeklyPlan && weeklyPlan.individualTasks.length > 4 && (
