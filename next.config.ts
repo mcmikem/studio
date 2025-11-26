662494de37c43c45bd958fb9f22bcf87e4c198ac
McMike M <1mark2mike@gmail.com>
2025-11-26 15:00:28 +0000

    Console logo
    Cloud Build
    /
    /
    Build details
    
    It looks like your build f

--- next.config.ts
+++ next.config.ts
@@ -36,13 +36,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview URL.
-    allowedDevOrigins: [
-      'https://*.cloudworkstations.dev',
-      'https://*.firebase.studio',
-    ],
-  },
 };
 
 export default nextConfig;
