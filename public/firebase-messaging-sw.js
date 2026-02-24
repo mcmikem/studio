9aac6100665a005237dda71d4de183635b1594cb
McMike M <1mark2mike@gmail.com>
2026-02-24 09:45:12 +0000

    Try fixing this error: `Console FirebaseError: Messaging: We are unable

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,32 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-import { getMessaging } from "firebase/messaging/sw";
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../src/firebase/config"; // Import the config
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseConfig = {
+  "projectId": "studio-3977002412-98a01",
+  "appId": "1:23348014627:web:05749dbde62724e11dcb96",
+  "storageBucket": "studio-3977002412-98a01.appspot.com",
+  "apiKey": "AIzaSyCM_fKRQuoAx2S7C3JPX4SPR5G6vv3uFJA",
+  "authDomain": "studio-3977002412-98a01.firebaseapp.com",
+  "measurementId": "G-1W5T5XJ41Q",
+  "messagingSenderId": "23348014627"
+};
 
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
+firebase.initializeApp(firebaseConfig);
 
-// Background Message Handler
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
+  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
